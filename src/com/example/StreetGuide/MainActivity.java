7229fcbb7df3b153abc7fbaec7b4845ae60c72da
pj <fake@server.com>
2013-05-26 22:30:06 +0100

    Opens map to show users current location Enabled GPS location so user can recentre map to current location

--- src/com/example/StreetGuide/MainActivity.java
+++ src/com/example/StreetGuide/MainActivity.java
@@ -1,18 +1,68 @@
 package com.example.StreetGuide;
 
 import android.app.Activity;
+import android.location.Location;
 import android.os.Bundle;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GooglePlayServicesClient;
+import com.google.android.gms.location.LocationClient;
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.LatLng;
 
-public class MainActivity extends Activity {
-    /**
-     * Called when the activity is first created.
-     */
+public class MainActivity extends Activity implements
+        GooglePlayServicesClient.ConnectionCallbacks,
+        GooglePlayServicesClient.OnConnectionFailedListener {
+
     private GoogleMap mMap;
+    private LocationClient mLocationClient;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        mMap = ((MapFragment) getFragmentManager().findFragmentById(R.id.map)).getMap();
+        mMap.setMyLocationEnabled(true);
+
+        mLocationClient = new LocationClient(this, this, this);
     }
 
+    @Override
+    protected void onStart() {
+        super.onStart();
+        // Connect the client.
+        mLocationClient.connect();
+    }
+
+    @Override
+    public void onConnected(Bundle bundle) {
+        //move to the users position
+        Location mCurrentLocation = mLocationClient.getLastLocation();
+
+        CameraPosition cameraPosition = new CameraPosition.Builder()
+                .target(new LatLng(mCurrentLocation.getLatitude(), mCurrentLocation.getLongitude()))     // Sets the center of the map to Mountain View
+                .zoom(15)                   // Sets the zoom
+                .tilt(20)                   // Sets the tilt of the camera to 30 degrees
+                .build();                   // Creates a CameraPosition from the builder
+        mMap.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
+    }
+
+    @Override
+    public void onDisconnected() {
+        //TODO
+    }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult connectionResult) {
+        //TODO
+    }
+
+    @Override
+    protected void onStop() {
+        // Disconnecting the client invalidates it.
+        mLocationClient.disconnect();
+        super.onStop();
+    }
 }
